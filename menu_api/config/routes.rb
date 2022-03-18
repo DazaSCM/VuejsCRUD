23f1ba338e7467fce4f8e2dda4cdb9febba55f56
DazaSCM <scm.thuradaza@gmail.com>
2022-03-18 10:46:49 +0630

    remove to fix rails-api application

--- menu_api/config/routes.rb
+++
@@ -1,6 +0,0 @@
-Rails.application.routes.draw do
-  get 'get_all_users', to: 'secret_menu_items#get_all_users'
-  post 'sign_up', to: 'secret_menu_items#sign_up'
-  post 'sign_in', to: 'secret_menu_items#sign_in'
-  resources :secret_menu_items, only: [:index, :show, :create, :update, :destroy]
-end
